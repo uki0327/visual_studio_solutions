52866a8c4d8f5bf114ac7cf359371fce294d58d9
uki0327 <uki0327@gmail.com>
2020-12-23 12:11:47 +0900

    simple gui uart serial communication

---
+++ WindowsSerialDataSendReceive/WindowsSerialDataSendReceive/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsSerialDataSendReceive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox_received = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(365, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 133);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox_received
+            // 
+            this.richTextBox_received.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox_received.Name = "richTextBox_received";
+            this.richTextBox_received.Size = new System.Drawing.Size(347, 389);
+            this.richTextBox_received.TabIndex = 1;
+            this.richTextBox_received.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 413);
+            this.Controls.Add(this.richTextBox_received);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "WindowsSerialDataSDendReceive";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+    }
+}
+
---
+++ WindowsSerialDataSendReceive/WindowsSerialDataSendReceive/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace WindowsSerialDataSendReceive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bool isConnect = serialConnect();
+            if (!isConnect)
+            {
+                MessageBox.Show("SerialPort Open Error");
+            }
+            else
+            {
+                MessageBox.Show("SerialPort Open OK");
+            }
+
+        }
+
+        public bool serialConnect()
+        {
+            serialPort.PortName = "COM1";
+            serialPort.BaudRate = 9600;
+            serialPort.DataBits = 8;
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.RtsEnable = false;
+            serialPort.DtrEnable = false;
+            try
+            {
+                serialPort.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(MySerialReceived));
+        }
+
+        private void MySerialReceived(object s, EventArgs e)  //여기에서 수신 데이타를 사용자의 용도에 따라 처리한다.
+        {
+            int ReceiveData = serialPort.ReadByte();  //시리얼 버터에 수신된 데이타를 ReceiveData 읽어오기
+            richTextBox_received.Text = richTextBox_received.Text + string.Format("{0:X2}", ReceiveData);  //int 형식을 string형식으로 변환하여 출력
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] datas = new byte[255];
+            datas[0] = 0x31;
+            if (serialPort.IsOpen) serialPort.Write(datas, 0, 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
